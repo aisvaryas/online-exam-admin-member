7062a61caa75bc6892d74866da7e3120d9bb46ce
aisvaryas <aisvarya.sun@gmail.com>
2026-03-27 09:51:48 +0530

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,13 +1,13 @@
 public class Member{
-    int mid;
+    
     String name,email;
 
-    public Member{
+    public Member(){
 
     }
 
-    public Member(int mid, String name, String email) {
-        this.mid = mid;
+    public Member( String name, String email) {
+        
         this.name = name;
         this.email = email;
     }
